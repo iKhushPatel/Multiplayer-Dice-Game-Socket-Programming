5ae02d4b3b3944128551ee8cc92a8339c93d856a
Khush Patel <ikhushpatel@gmail.com>
2020-04-18 12:16:11 -0400

    Update client.c

--- client.c
+++ client.c
@@ -1,55 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<time.h>
+#include<string.h>
 
-#define PORT 4444
 
-int main(){
-
-	int clientSocket, ret;
-	struct sockaddr_in serverAddr;
-	char buffer[1024];
-
-	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket < 0){
-		printf("[-]Error in connection.\n");
+int main(int argc, char *argv[]){
+	char message[100];
+	int server, portNumber;
+	socklen_t len;
+	struct sockaddr_in servAdd;
+	int dice;
+	int32_t point;
+	int size=sizeof(point);
+	long int ss=0;
+	if(argc != 3){
+		printf("Call model:%s <IP> <Port#>\n",argv[0]);
+		exit(0);
+	}
+	if ((server=socket(AF_INET,SOCK_STREAM,0))<0){
+		fprintf(stderr, "[-] Cannot create socket\n");
 		exit(1);
 	}
-	printf("[+]Client Socket is created.\n");
-
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
+	else
+	{
+		fprintf(stderr, "[+] Socket Created\n");
 	}
-	printf("[+]Connected to Server.\n");
-
+	
+	servAdd.sin_family = AF_INET;
+	sscanf(argv[2], "%d", &portNumber);
+	servAdd.sin_port = htons((uint16_t)portNumber);
+	if(inet_pton(AF_INET, argv[1],
+		&servAdd.sin_addr) < 0){
+		fprintf(stderr, " inet_pton() has failed\n");
+		exit(2);
+	}
+	if(connect(server, (struct sockaddr *) &servAdd,
+		sizeof(servAdd))<0){
+		fprintf(stderr, "connect() failed, exiting\n");
+		exit(3);
+	}
 	while(1){
-		printf("Client: \t");
-		scanf("%s", &buffer[0]);
-		send(clientSocket, buffer, strlen(buffer), 0);
-
-		if(strcmp(buffer, ":exit") == 0){
-			close(clientSocket);
-			printf("[-]Disconnected from server.\n");
-			exit(1);
+		if (read(server, message, sizeof(message))<0){ // sizeof(message)
+			fprintf(stderr, "read() error\n");
+			exit(0);
 		}
-
-		if(recv(clientSocket, buffer, 1024, 0) < 0){
-			printf("[-]Error in receiving data.\n");
-		}else{
-			printf("Server: \t%s\n", buffer);
+		printf("%s\n",message);
+		if(!strcmp(message,"You can now play")){
+			dice = (int) time(&ss)%6+1;
+			printf("********** You got: %d **********\n",dice);
+			point=htonl(dice);
+			write(server,&point,size);
 		}
 	}
-	return 0;
+	exit(0);
 }
